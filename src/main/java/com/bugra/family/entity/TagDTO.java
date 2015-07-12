f270e0c6c83147e18be4461b145d196b3f49a7a6
Abdulkerim Bugra Cavdar <bugrizm@gmail.com>
2015-07-12 18:54:43 +0300

    etiket ekleme

---
+++ src/main/java/com/bugra/family/entity/TagDTO.java
@@ -0,0 +1,36 @@
+package com.bugra.family.entity;
+
+
+public class TagDTO {
+	
+	private Integer id;
+	private String name;
+	private String iconText;
+	private String color;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getIconText() {
+		return iconText;
+	}
+	public void setIconText(String iconText) {
+		this.iconText = iconText;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+    
+	
+}
